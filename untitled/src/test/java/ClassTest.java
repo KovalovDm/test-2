2ff44ce7e2eaec69575a118607a7e3166699d9f5
Dmytro Kovalov <kovaldmy@fel.cvut.cz>
2023-05-03 09:54:30 +0200

    second commit

---
+++ untitled/src/test/java/ClassTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ClassTest {
+    @Test
+    void anytext() {
+        Assertions.assertTrue((new int[3]).length == 3);
+    }
+}
